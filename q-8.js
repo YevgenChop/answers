93b85e8726837f8e864414c06631c60dff35924e
YevgenChop <44809341+YevgenChop@users.noreply.github.com>
2020-02-17 19:19:41 +0200

    Create q-8.js

---
+++ q-8.js
@@ -0,0 +1,6 @@
+/* question:
+What is your prefered layout system: inline-block, floating + clearing, flex, grid, other?  And why?
+
+answer:
+I prefer such system as Flex because it is  very simple and predictable model.
+*/
