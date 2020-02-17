e250c344964100f3b7d5dbcdb2c2d411e803de86
YevgenChop <44809341+YevgenChop@users.noreply.github.com>
2020-02-17 19:17:03 +0200

    Create q-5.js

---
+++ q-5.js
@@ -0,0 +1,6 @@
+/* question:
+In your own words, explain what a javascript ‘class’ is and how it differs from a function.
+
+answer:
+This is syntactic sugar for the convenience of working with constructor functions.
+*/
