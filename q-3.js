1e537dc17a16205e50e9f22a07c81cf793b509c9
YevgenChop <44809341+YevgenChop@users.noreply.github.com>
2020-02-17 19:14:26 +0200

    Create q-3.js

---
+++ q-3.js
@@ -0,0 +1,10 @@
+
+
+/* question: 
+Is there any difference between regular function syntax and the shorter arrow function syntax? 
+(Write the answer in your own words)
+
+answer:
+The difference in the “this”. 
+Arrow function do not have their own context, they get the context of the place where they were created.
+*/
