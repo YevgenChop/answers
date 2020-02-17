28e5af70b7791d5d452e712a1a4d0c627c83a458
YevgenChop <44809341+YevgenChop@users.noreply.github.com>
2020-02-17 19:22:57 +0200

    Create q-12.js

---
+++ q-12.js
@@ -0,0 +1,7 @@
+/* question:
+Are there any pitfalls associated with this technology that have caused you difficulty in the past?
+
+answer:
+Yes, when using CSS-in-JS technologies, you cannot select DOM elements by class name or ID. 
+Which makes it difficult to search for DOM elements.
+*/
